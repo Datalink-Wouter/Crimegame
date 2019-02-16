e907faac296ebb64948bb0c7afac49f64cf9debc
Wouter Dendas <wouter@datalink.be>
2019-02-16 16:30:54 +0100

    new layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Crimegame') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <header>
+
+        </header>
+
+        <main class="py-4">
+            @include('alert::bootstrap')
+            @yield('content')
+        </main>
+    </div>
+
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
